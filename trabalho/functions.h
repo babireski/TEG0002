7eff030f95a2e978a9c3dfe017db928b1eb68185
13189385971 <13189385971@edu.udesc.br>
2022-09-22 12:30:29 -0300

    Read function (almost)

--- trabalho/functions.h
+++ trabalho/functions.h
@@ -16,14 +16,57 @@ float normalize(float x, float maximum, float minimum)
     return (x - minimum) / (maximum - minimum);
 }
 
-void read(sample samples[])
+sample* newSamples() {
+    sample* samples = (sample *) malloc(sizeof(sample*));
+    return samples;
+}
+
+// All the samples read will be stored in samples pointer, which
+// will be changed by reference, and while reading the database, 
+// the maximum and minimum will be calculated, and also changed by reference
+void read(sample **samples, int *maximum, int *minimum)
 {
+    *samples = newSamples();
     FILE *dataset = fopen("dataset.csv", "r");
 
-    if(dataset)
+    if(dataset == NULL)
     {
-
+        printf("Erro ao carregar arquivo.\n");
+        exit(-1);
     }
+
+    char* variety = (char*) malloc(sizeof(char*));
+    char* firstLine = (char*) malloc(sizeof(char) * 100);
+    int i = 0;
+
+    // Discard first line
+    fgets(firstLine, 100, dataset);
+    free(firstLine);
+    while (fscanf(dataset, "%f,%f,%f,%f,%s\n", &(*samples)[i].sepal.length, &(*samples)[i].sepal.width, &(*samples)[i].petal.length, &(*samples)[i].petal.width, variety))
+    {
+        printf("%.1f, %.1f\n", (*samples)[i].sepal.length, (*samples)[i].sepal.width);
+        if (strcmp(variety, "\"Setosa\"") == 0)
+        {
+            (*samples)[i].species = 1;
+        }
+        else if (strcmp(variety, "\"Versicolor\"") == 0)
+        {
+            (*samples)[i].species = 2;
+        }
+        else if (strcmp(variety, "\"Virginica\"") == 0)
+        {
+            (*samples)[i].species = 3;
+        }
+        else
+        {
+            printf("Especie nao reconhecida. Abortando\n");
+            exit(-1);
+        }
+        // mallocating a bit wrong, need to find out why.
+        // the rest aparently is working fine
+        samples[i++] = (sample*) malloc(sizeof(sample) + 1);
+    }
+
 }
 
 void load()
---
+++ trabalho/main.c
@@ -0,0 +1,12 @@
+#include "functions.h"
+
+int main(void)
+{
+    sample* samples = NULL;
+    int maximum = 0;
+    int minimum = maximum;
+
+    read(&samples, &maximum, &minimum);
+
+    return 0;
+}
