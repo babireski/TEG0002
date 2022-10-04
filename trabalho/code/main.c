55b72f71736ecf76840f6635b743083a10edd251
babireski <elian.babireski@gmail.com>
2022-10-04 11:05:13 -0300

    File functions.c updated

--- trabalho/code/main.c
+++ trabalho/code/main.c
@@ -2,19 +2,10 @@
 
 int main(void)
 {
-	sample* samples = NULL;
-	int maximum = 0;
-	int minimum = maximum;
-
-	readCsv(&samples);
-
-	printf("sepLen\tsepWid\tpetLen\tpetWid\n");
-
-	for (int i = 0; i < numSamples; i++)
-	{
-		printf("%.1f\t%.1f\t%.1f\t%.1f\t%d\n", samples[i].sepal.length, samples[i].sepal.width, samples[i].petal.length, samples[i].petal.width, samples[i].species);
-	}
-	free(samples);
-
+	graph *descriptor = create();
+	read(descriptor);
+	link(descriptor, table(descriptor));
+	print(descriptor);
+	
 	return 0;
 }
